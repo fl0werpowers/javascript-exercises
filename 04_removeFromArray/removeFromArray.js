f5aff1c52142c40b8b1823076f86d334fbebb73a
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-11-01 19:06:44 +0100

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...rmArgs) => {
+	let resultArr = arr;
+	for (const arg of rmArgs) {
+		resultArr = resultArr.filter((arrElement) => arrElement !== arg);
+	}
+
+	return resultArr;
 };
 
 // Do not edit below this line
