962f7352b00e7746872b93cd81df1e0e04b2a3c2
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-11-01 18:53:37 +0100

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+	let resString = "";
+	for (let i = string.length; i > 0; i--) {
+		resString += string.at(i - 1);
+	}
+	return resString;
 };
 
 // Do not edit below this line
