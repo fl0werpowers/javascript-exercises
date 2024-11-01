ac24d9163cc79ddd8eaeb59098c9cfda42581dd1
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-11-01 19:25:27 +0100

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+	if (!isPositiveInteger(num1) || !isPositiveInteger(num2)) {
+		return "ERROR";
+	}
 
+	const args = num1 < num2 ? [num1, num2] : [num2, num1];
+
+	let res = 0;
+
+	for (let i = args[0]; i <= args[1]; i++) {
+		res += i;
+	}
+
+	return res;
 };
+
+function isPositiveInteger(number) {
+	return typeof number === "number" && number >= 0 && Number.isInteger(number);
+}
 
 // Do not edit below this line
 module.exports = sumAll;
