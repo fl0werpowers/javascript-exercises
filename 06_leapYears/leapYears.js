a9e0d084b94f31e5cd3020ae4232c5dd3fbe2e8d
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-11-01 19:36:54 +0100

    Solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
+const leapYears = (year) => {
+	if (year % 100 === 0 && year % 400 !== 0) return false;
 
+	return year % 4 === 0;
 };
 
 // Do not edit below this line
