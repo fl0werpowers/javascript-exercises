0f8adf99bd709c1fc79a0808c6dc08171bcf0c8a
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-12-10 15:41:28 +0100

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = (str) => {
+	const filteredStr = str
+		.toLowerCase()
+		.split("")
+		.filter((ch) => ch.match(/^[0-9a-z]+$/))
+		.join("");
 
+	return filteredStr === filteredStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
