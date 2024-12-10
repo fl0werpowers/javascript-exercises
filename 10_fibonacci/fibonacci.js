f07caa0a4853048d497fe9b83fc36434b63470ba
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-12-10 15:59:29 +0100

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = (index) => {
+	if (index < 0) return "OOPS";
+	// biome-ignore lint/suspicious/noDoubleEquals: This function accepts strings as well
+	if (index == 0) return 0;
+	let fibo1 = 1;
+	let fibo2 = 1;
+	for (let i = index; i > 2; i--) {
+		const fibo3 = fibo1 + fibo2;
+		fibo1 = fibo2;
+		fibo2 = fibo3;
+	}
+	return fibo2;
 };
 
 // Do not edit below this line
