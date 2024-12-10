7ebb6c6c9c1a09eab635b0747a0b8d85df5e7f97
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-12-10 16:16:48 +0100

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+	const personAge = (person) => {
+		return person.yearOfDeath
+			? person.yearOfDeath - person.yearOfBirth
+			: new Date().getFullYear() - person.yearOfBirth;
+	};
+	return people.sort((person1, person2) =>
+		personAge(person1) > personAge(person2) ? -1 : 1,
+	)[0];
 };
 
 // Do not edit below this line
